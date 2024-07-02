c4cf6b5e6acf6ceafca2846a55cba0db89dbea5b
= Adrián López <=adrian@imagendigital.com>
2024-07-02 09:31:09 -0600

    Update ( picsum ) : cambiando parametros.

--- src/components/Carrusel/logic.js
+++ src/components/Carrusel/logic.js
@@ -1,71 +1,48 @@
-import { useEffect, useState , useRef } from "react";
+import { useEffect, useState, useRef } from "react";
 
 // https://picsum.photos/
-let GetImages = async function (){
-    return fetch( "https://picsum.photos/v2/list").then(( respond )=>{
-       return respond.json(); 
-    }).then( ( data )=>{  
-     return data;  
+let GetImages = async function () {
+    return fetch("https://picsum.photos/v2/list?limit=5&page=3").then((respond) => {
+        return respond.json();
+    }).then((data) => {
+        return data;
     })
-  
-  }
+
+}
 
 
-function Controls( ) {
+function Controls() {
     let [index, setIndex] = useState(0);
-    let [ Images, setImages ] = useState( [] );
- 
-
-
-
-
-    useEffect(()=>{
-      
-        GetImages().then( ( data )=>{
-         
-             setImages( data ) ;
-
+    let [Images, setImages] = useState([]);
+    useEffect(() => {
+        GetImages().then((data) => {
+            setImages(data);
         })
 
-    },[])
+    }, []);
 
-
-    
     const Max = Images.length - 1 || 0;
-    let nextIndex =   ( Max == index) ? 0 : index + 1;
-    let beforeIndex =   ( index == 0 ) ? Max : index - 1;
- 
-
-
-    let SendRender = ()=>{
-        setRender( ()=>{
-            return !render
-        })
-
-    }
-
-   
-
-
+    let nextIndex = (Max == index) ? 0 : index + 1;
+    let beforeIndex = (index == 0) ? Max : index - 1;
     let stack = Images;
-  
+
     return {
         index: index,
-        CurrentImg : stack[ index ],
-        nextImg : stack[ nextIndex ],
-        beforeImg : stack[ beforeIndex ],
+        CurrentImg: stack[index],
+        nextImg: stack[nextIndex],
+        beforeImg: stack[beforeIndex],
         nextImage: () => {
-            
-            setIndex( nextIndex);
-       
+
+            setIndex(nextIndex);
+
         },
 
         PreviusImage: () => {
 
-           setIndex( beforeIndex );
+            setIndex(beforeIndex);
 
-        } 
-        
+        }
+
 
     }
 }
