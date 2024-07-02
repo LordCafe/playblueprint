6d6cea2bcc44ef742bfb8f584ccb8d669ab8c6e4
= Adrián López <=adrian@imagendigital.com>
2024-07-01 18:39:35 -0600

    Build ( mejorando ) : separando componentes footer y header

--- src/app/section/page.js
+++ src/app/section/page.js
@@ -1,7 +1,15 @@
-
+import styles from "../page.module.css"
+import Image from "next/image";
+import Header from "@/components/header/header";
+import Footer from "@/components/footer/footer";
 
 export default function Home() {
-  return (
-    <div>  Hola mundo </div>
-  );
+    return (
+
+        <main className={styles.main}>
+            <Header></Header>            
+
+            <Footer></Footer>
+        </main>
+    );
 }
